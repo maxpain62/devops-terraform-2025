46f5b6b2aeaba41c7be4b0bb913f5325c2ed6a80
gauravbhatkar678 <maxpain62@gmail.com>
2025-07-07 21:32:21 +0530

    minor change

--- terraform/auth and state filr/main.tf
+++ terraform/auth and state filr/main.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 resource "aws_instance" "firstinstance" {
-  ami           = "ami-0d03cb826412c6b0f"
+  ami           = ami-0f918f7e67a3323f0
   instance_type = "t2.micro"
   tags = {
     Name      = "Helloworld"
